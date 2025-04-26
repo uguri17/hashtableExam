3cf5756cb51a34316e95e8b5f7f2537841858f32
osbor <osbor@DESKTOP-EPCSFA4>
2025-04-26 22:34:31 +0900

    초기 커밋.

---
+++ MainApp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using static System.Console;
+
+namespace hashtableExam
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            // 회사 : Microsoft
+            // URL : www.ms.com
+
+            // <Hashtable init 1>
+            /*Hashtable ht = new Hashtable();
+            ht["회사"] = "Microsoft";
+            ht["URL"] = "www.microsoft.com";*/
+
+            // <Hashtable init 2>
+            Hashtable ht = new Hashtable()
+            {
+                { "회사", "Microsoft" },
+                { "URL", "www.microsoft.com"}
+            };
+
+            foreach (DictionaryEntry entry in ht)
+            {
+                WriteLine($"{entry.Key} : {entry.Value}");
+            }
+                
+        }
+    }
+}
